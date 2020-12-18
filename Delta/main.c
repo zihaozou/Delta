9dbd6c4c01aef8f561f203f5c0dfa7d13787c80a
zihao <986007000@qq.com>
2020-12-18 10:38:09 +0800

    Initial Commit

---
+++ Delta/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Delta
+//
+//  Created by 邹子豪 on 12/18/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
